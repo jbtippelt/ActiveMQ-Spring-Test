cf9c0b4d5daf85504a2110a0c4f31b7050e3e5b0
Jakob Tippelt <st144996@stud.uni-stuttgart.de>
2018-06-14 11:18:29 +0200

    Changed producer - post to add user /  get to trigger request-receive

--- activeMQProducer/src/main/java/de/jakob/activeMQProducer/Producer.java
+++ activeMQProducer/src/main/java/de/jakob/activeMQProducer/Producer.java
@@ -6,43 +6,69 @@ import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.jms.*;
 import java.util.Queue;
+import java.util.Random;
 
 @RestController
-@RequestMapping("/produce/")
+@RequestMapping("/")
 public class Producer {
 
     @Autowired
     JmsTemplate jmsTemplate;
 
     @Autowired
-    Queue queue;
+    Destination addDestination;
 
-    @GetMapping("/{username}")
-    public String publish(@PathVariable("username")
-                          final String username) throws JMSException, JsonProcessingException {
+    @Autowired
+    Destination getRequestDestination;
 
-        UserObject user = new UserObject();
-        user.setName(username);
-        user.setAge(45);  // constant age, just for example
+    @Autowired
+    ConnectionFactory connectionFactory;
+
+    @PostMapping("/add")
+    ResponseEntity<?> add(@RequestBody UserObject user) throws JsonProcessingException {
 
         ObjectMapper mapper = new ObjectMapper();
         String userAsString = mapper.writeValueAsString(user);
 
+        jmsTemplate.convertAndSend(addDestination, userAsString);
 
-        jmsTemplate.convertAndSend("my-queue", userAsString);
+        return ResponseEntity.ok("Produced successfully: " +  userAsString);
+    }
 
-        return "Produced successfully: " +  userAsString;
+
+    @GetMapping("/get/{index}")
+    public String request(@PathVariable("index")
+                          final int index) throws JMSException, JsonProcessingException {
+
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination replyDestination = session.createTemporaryQueue();
+
+        Message message = session.createMessage();
+        message.setIntProperty("index", index);
+        message.setJMSReplyTo(replyDestination);
+        message.setJMSCorrelationID(Long.toHexString(new Random(System.currentTimeMillis()).nextLong()));
+
+        jmsTemplate.convertAndSend(getRequestDestination, message);
+
+        MessageConsumer consumer = session.createConsumer(replyDestination);
+        TextMessage reply = (TextMessage)consumer.receive();
+        System.out.println("RECEIVED: "  + reply.getText());
+
+        session.close();
+
+        return "Received successfully: " +  reply.getText();
     }
 }
